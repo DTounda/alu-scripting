56fe4bcd0f6375818b9afb7291faff7362bf3227
DTounda <toundanana@gmail.com>
2026-06-29 15:09:52 +0200

    add api_advanced reddit subscriber function

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Returns the number of subscribers for a given subreddit."""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """Query Reddit API and return subscriber count for a subreddit."""
     url = (f"https://www.reddit.com/r/{subreddit}/about.json")
     custom_headers = {"User-Agent": "MyApp/1.0"}
     response = requests.get(url, headers=custom_headers, allow_redirects=False)
